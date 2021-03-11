2e184b56cd7a1be5f050845148e9ee752d8ec358
al0 <luca.aeberhard@suva.ch>
2021-03-11 11:56:30 +0100

    Login with Name and Dashboard as Home-Directory

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,10 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Dashboard');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
