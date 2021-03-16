b88c8742ebbef793c49ad3963e3403dbd87d58d4
Nicola <nicola@fioretti.info>
2021-03-16 10:11:16 +0100

    Questions are now displayed for answering

--- resources/js/Pages/AnswerBasicSurvey/AnswerTerminQuestion.vue
+++ resources/js/Pages/AnswerBasicSurvey/AnswerTerminQuestion.vue
@@ -2,17 +2,20 @@
 
         <div class="flex flex-col mb-4 md:mx-10">
             <div class="mb-2 text-grey-darkest">{{question.name}}</div>
+
+            <div class="flex flex-col mb-4 md:mx-5 space-y-6" >
+                <div v-for="(option,index) in question.termins">
+                    <termin-ticker :option="option" :answer="answer">
+
+                    </termin-ticker>
+                </div>
+
+            </div>
+
+
         </div>
-    {{question}}
 
-    <div class="flex flex-col mb-4 md:mx-5 space-y-6" >
-        <div v-for="(option,index) in question.termins">
-            <termin-ticker :option="option" :answer="answer">
 
-            </termin-ticker>
-        </div>
-
-    </div>
 </template>
 
 <script>
--- resources/js/Pages/AnswerBasicSurvey/TerminTicker.vue
+++ resources/js/Pages/AnswerBasicSurvey/TerminTicker.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="mb-2 text-grey-darkest">{{option.time + '   Dauer:' +  option.duration}}</div>
-    <input type="checkbox" v-model="answer[option.id]" class="form-checkbox">
+    <div class="flex flex-col">
+        <label class="inline-flex items-center mt-3">
+            <input type="checkbox"  v-model="answer[option.id]" class="form-checkbox h-5 w-5 text-blue-600"><span class="ml-2 text-gray-700">
+            {{terminDate.getDate() + '.' +(terminDate.getMonth() +1) + '.' + terminDate.getFullYear()+ '   '+terminDate.getHours()+':'+terminDate.getMinutes()+'  Dauer: ' +  option.duration +' minuten'}}
+        </span>
+        </label>
+    </div>
 </template>
 
 <script>
@@ -9,7 +14,7 @@ export default {
     props: ['option','answer'],
     data: function (){
         return {
-
+            terminDate: new Date(this.option.time),
         }
     },
     beforeCreate() {
