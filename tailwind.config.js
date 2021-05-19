6c7f1189eebbd7f43dcbd62dc0f2b443dfc3ece8
Nicola <nicola@fioretti.info>
2021-05-19 10:44:58 +0200

    Css-Edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,38 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                create: {
+                    DEFAULT: '#4A732E',
+                    hover: '#356517',
+                    border: '#215600',
+                    boarderhover: '#356517',
+                },
+                answer: {
+                    DEFAULT: '#F36423',
+                    hover: '#F5541C',
+                    border: '#F64C18',
+                    boarderhover: '#F5541C',
+                },
+                result: {
+                    DEFAULT: '#3C94D3',
+                    hover: '#298CCB',
+                    border: '#1784C3',
+                    boarderhover: '#055787',
+                },
+                live: {
+                    DEFAULT: '#3A32C2',
+                    hover: '#283AD9',
+                    border: '#422DB7',
+                    boarderhover: '#4B29AB',
+                },
+                host: {
+                    DEFAULT: '#8B1D17',
+                    hover: '#9D221E',
+                    border: '#79180F',
+                    boarderhover: '#661308',
+                },
+            },
         },
     },
 
